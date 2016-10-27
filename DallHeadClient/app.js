a8c2516087e2509d4bdbf2d9cdb3587b7fbffb2a
James Dickinson <jimmy@jimmyinteractive.com>
2016-10-27 00:18:09 -0700

    first

---
+++ DallHeadClient/app.js
@@ -0,0 +1,10 @@
+﻿console.log('starting...');
+
+var socket = require('socket.io-client')('http://dollheadweb.azurewebsites.net');
+socket.on('connect', function () { });
+socket.on('command', function (command, args) {
+    if (command === "sound") {
+
+    }
+});
+socket.on('disconnect', function () { });
---
+++ DollHeadWeb/server.js
@@ -0,0 +1,30 @@
+﻿var http = require('http');
+var express = require('express');
+var ioServer = require('socket.io');
+
+var port = process.env.port || 1337;
+var version = "0.1.0";
+
+//express server routing
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+//http server 
+var httpServer = http.createServer(app);
+httpServer.listen(port, function () { console.log('***Server(http) listening at port %d *** version %s', port, version); });
+
+
+//socket io server
+var io = new ioServer();
+io.attach(httpServer);
+io.on('connection', ioConnection);
+
+function ioConnection(socket) {
+    socket.on("command", function (command, args) {
+        io.sockets.emit('command', command, args);
+    });
+};
