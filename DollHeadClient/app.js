48a42e3548eb1bbf862e6e135ed3159a2ff09ebf
jamesfdickinson <jamesfdickinson@gmail.com>
2016-10-27 15:00:24 -0700

    added motor code

--- DollHeadClient/app.js
+++ DollHeadClient/app.js
@@ -1,9 +1,13 @@
 ﻿console.log('starting...');
 var fartList = require('./fartList.js');
-
+var StepperWiringPi = require("stepper-wiringpi");
 var socket = require('socket.io-client')('http://dollheadweb.azurewebsites.net');
 var Sound = require('aplay');
 var player = new Sound();
+
+//StepperWiringPi.setup(stepsInRevolution, pin1, pin2, pin3, pin4);
+var motor = StepperWiringPi.setup(360, 1, 2, 3, 4);
+motor.setSpeed(60);
 
 socket.on('connect', function () {
   console.log('Socket connected to server');
@@ -18,10 +22,16 @@ socket.on('command', function (command, args) {
       player.play(fartSound);
   }
   if (command === "turnRight") {
-	//todo: move motor
+	//move motor
+	motor.forward();
   }
   if (command === "turnLeft") {
-	//todo: move motor
+	//move motor
+	motor.backward();
+  }
+  if (command === "turnStop") {
+	//stop motor
+	motor.stop();
   }
   console.log('Done');
 });
