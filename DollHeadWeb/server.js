7d58328a2b1c4e489e119a22649fc374bb94dd37
Luke Dickinson <Lukeadickinson@yahoo.com>
2016-11-05 17:18:05 -0600

    asdf

--- DollHeadWeb/server.js
+++ DollHeadWeb/server.js
@@ -2,29 +2,25 @@
 var express = require('express');
 var ioServer = require('socket.io');
 
-var port = process.env.port || 80;
+var port = process.env.port || 1337;
 var version = "0.1.0";
 
 //express server routing
-var app = express()
-	, httpServer = http.createServer(app)
-	, ioServer = ioServer.listen(httpServer);
+var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
-console.log('123123');
+
 //http server 
-//var httpServer = http.createServer(app);
-//console.log('***Server(http) listening at port %d *** version %s', port, version);
+var httpServer = http.createServer(app);
+httpServer.listen(port, function () { console.log('***Server(http) listening at port %d *** version %s', port, version); });
 
 //socket io server
-//var io = new ioServer();
-//io.attach(httpServer);
-
-httpServer.listen(port, function () { console.log('***Server(http) listening at port %d *** version %s', port, version); });
-ioServer.on('connection', ioConnection);
+var io = new ioServer();
+io.attach(httpServer);
+io.on('connection', ioConnection);
 
 function ioConnection(socket) {
     socket.on("command", function (command) {
